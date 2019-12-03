f231ae1094ead1b29b1bd0c2040993be64146f12
Siberian Nerd <whatsgoin1@gmail.com>
2019-12-03 20:01:54 +0300

    Вывела построение очереди в отдельную функцию

--- gamebase_v0.1/battleround.py
+++ gamebase_v0.1/battleround.py
@@ -6,38 +6,37 @@ import battlemode as bm
 import random
 
 def Auto_Battle(player_team, enemy_team):
-    general_queue = bm.general_queue_logic(player_team, enemy_team)
-    print(general_queue, '\n')
     round_count = 1
-    battle_queue = []
-    for unit in general_queue:
-            if unit[0].alive:
-                battle_queue.append(unit)
-    round_queue = bm.Unique_initiative(battle_queue)
-    round_units_queue = [i[0] for i in round_queue]
     while player_team and enemy_team:
         print(round_count, 'Round! Begin!','\n')
+        round_units_queue =  general_round_queue(player_team, enemy_team)
         print(round_units_queue, '\n')
         for unit in round_units_queue:
             print(f"It's {unit.name}'s torn to go")
             defending_team = defence_turn(unit, player_team, enemy_team)
             enemy_unit = unit_turn(unit, defending_team)
             check_unit_from_team(enemy_unit, defending_team, round_units_queue)
-            if not player_team and enemy_team: break
+            if not (player_team and enemy_team): break
         print('End of the round ', round_count, '. \n')
         round_count +=1
 
+def general_round_queue(player_team, enemy_team):
+    general_queue = bm.general_queue_logic(player_team, enemy_team)
+    battle_queue = []
+    for unit in general_queue:
+            if unit[0].alive and (unit[0].initiative > 0):
+                battle_queue.append(unit)
+    round_queue = bm.Unique_initiative(battle_queue)
+    round_units_queue = [i[0] for i in round_queue]
+    return round_units_queue
+
 def unit_turn(unit, enemy_team):
     """
-    В чем идея:
-    взять повторяющийся код из верхней части и вывести его в отдельную функцию. В итоге в главной функции проверка идет на то,
-    к какой команде принадлежит юнит, и все.
-    В этой функции юнит будет ходить автоматически, возвращаться будет результат.
-    Главная функция будет обрабатывать результат.
+    Автоход юнита. Прототип.
     """
     enemy_unit = random.choice(enemy_team)
-    enemy_unit.health = bm.single_target_attack(unit.Attack(), enemy_unit.health)
-    print(f'{enemy_unit.name} got {enemy_unit.health} HP left')
+    enemy_unit.current_health = bm.single_target_attack(unit.Attack(), enemy_unit.current_health)
+    print(f'{enemy_unit.name} got {enemy_unit.current_health} HP left')
     enemy_unit.Is_alive()
     return enemy_unit
     
