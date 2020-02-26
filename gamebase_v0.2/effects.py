5651f7dd6a66359dec0929aba41f73b52b3dba25
Siberian Nerd <whatsgoin1@gmail.com>
2020-02-26 20:07:01 +0300

    добавила модуль эффектов

--- gamebase_v0.2/effects.py
+++ gamebase_v0.2/effects.py
@@ -1,49 +1,64 @@
 """
 Содержит все эффекты и состояния, которые могут применятся к юнитам в бою
+
+Есть два типа эффектов:
+    постоянные
+    прогрессивные
+у постоянных эффектов передаются:
+    базовое значение
+    измененное значение - ссылка на измененный аттрибут юнита
+    продолжительность
+у прогрессивных эффектов передаются:
+    тип эффекта
+    базовое значение
+    модификатор
+    продолжительность
+
+Прогрессивные эффекты накладываются в начале хода, продолжительность эффектов уменьшается в конце хода.
 """
 
-# POSITIVE EFFECTS
+from collections import namedtuple as ntp
 
-def StrenghtBuff(unit):
-    pass
+class ConstantEffect():
+    def __init__(self, effect):
+        self.base = effect[0]
+        self.amount = effect[1]
+        self.duration = effect[2]
 
-def InitiativeBuff(unit):
-    pass
+class ProgressiveEffect():
+    def __init__(self, effect):
+        self.type = effect[0]
+        self.base = effect[1]
+        self.amount = effect[2]
+        self.duration = effect[3]
+        
+def GetEffectInfo(effect):
+    """Делает из списка с описанием эффекта именованый список для удобства работы с его аттрибутами
+    
+    Arguments:
+        effect {list} -- список с аттрибутами эффекта
+    
+    Returns:
+        namedtuple -- именованый список
+    """
+    if effect[0] == 'constant':
+        current_effect = ConstantEffect(effect)
+    elif effect[0] == 'progressive':
+        current_effect = ProgressiveEffect(effect)
+    return current_effect
 
-
-
-# END OF POSITIVE
-
-
-
-# NEGATIVE EFFECTS
-
-def StrenghtDebuff(unit):
-    pass
-
-def InitiativeDebuff(unit):
-    pass
-
-def Stun(unit):
-    '''
-    Sets Initiative = 0
-    '''
-    unit.Initiative = 0
-
-def Bleeding(unit):
-    '''
-    Deals damage every turn.
-    '''
-    pass
-
-# END OF NEGATIVE EFFECTS
-
-
-
-def ApplyEffects(unit):
-    for effect in unit.effects:
-        if effect == 'stun':
-            Stun(unit)
-        if effect == 'Bleeding':
-            Bleeding(unit)
-            
+def ApplyEffect(effect):
+    if effect[0] == 'constant':
+        current_effect = GetEffectInfo(effect)
+    elif effect[0] == 'progressive':
+        current_effect = GetEffectInfo(effect)
+        ApplyProgressiveEffect(current_effect.type, current_effect.base, current_effect.amount)
+    current_effect.duration -= 1
+   
+def ApplyProgressiveEffect(ef_type, current_amount, mod_amount):
+    possible_effects = {
+    'buff': lambda: current_amount + mod_amount,
+    'debuff': lambda: current_amount - mod_amount,
+    }
+    res_amount = possible_effects.get(ef_type, lambda: None)()
+    return res_amount
