21ab4796605ae9491dd37254b9d47d7e3d79d124
Siberian Nerd <whatsgoin1@gmail.com>
2020-02-28 23:19:22 +0300

    поправила действие эффектов

--- gamebase_v0.2/effects.py
+++ gamebase_v0.2/effects.py
@@ -2,17 +2,19 @@
 Содержит все эффекты и состояния, которые могут применятся к юнитам в бою
 
 Есть два типа эффектов:
-    постоянные
-    прогрессивные
-у постоянных эффектов передаются:
-    базовое значение
-    измененное значение - ссылка на измененный аттрибут юнита
-    продолжительность
+    * постоянные
+    * прогрессивные
+
+У постоянных эффектов передаются:
+    * базовое значение
+    * название измененного аттрибута
+    * продолжительность
+
 у прогрессивных эффектов передаются:
-    тип эффекта
-    базовое значение
-    модификатор
-    продолжительность
+    * тип эффекта
+    * базовое значение
+    * модификатор
+    * продолжительность
 
 Прогрессивные эффекты накладываются в начале хода, продолжительность эффектов уменьшается в конце хода.
 """
@@ -21,16 +23,16 @@ from collections import namedtuple as ntp
 
 class ConstantEffect():
     def __init__(self, effect):
-        self.base = effect[0]
-        self.amount = effect[1]
-        self.duration = effect[2]
+        self.base = effect[1]
+        self.attr = effect[2]
+        self.duration = effect[3]
 
 class ProgressiveEffect():
     def __init__(self, effect):
-        self.type = effect[0]
-        self.base = effect[1]
-        self.amount = effect[2]
-        self.duration = effect[3]
+        self.ftype = effect[1]
+        self.base = effect[2]
+        self.amount = effect[3]
+        self.duration = effect[4]
         
 def GetEffectInfo(effect):
     """Делает из списка с описанием эффекта именованый список для удобства работы с его аттрибутами
@@ -48,12 +50,16 @@ def GetEffectInfo(effect):
     return current_effect
 
 def ApplyEffect(effect):
+    """Накладывает на юнита выбранный эффект.
+    
+    Arguments:
+        effect {list} --   Список значений выбранного эффекта
+    """
     if effect[0] == 'constant':
         current_effect = GetEffectInfo(effect)
     elif effect[0] == 'progressive':
         current_effect = GetEffectInfo(effect)
-        ApplyProgressiveEffect(current_effect.type, current_effect.base, current_effect.amount)
-    current_effect.duration -= 1
+        ApplyProgressiveEffect(current_effect.ftype, current_effect.base, current_effect.amount)
    
 def ApplyProgressiveEffect(ef_type, current_amount, mod_amount):
     possible_effects = {
